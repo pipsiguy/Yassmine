746cb1f63011c40b577181edec83deac35c41b1d
pipsiguy <152687387+pipsiguy@users.noreply.github.com>
2026-02-24 02:44:18 +0000

    hi

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-static";
 import { MetadataRoute } from 'next';
 import { getPosts } from "@/utils/utils";
 import { baseURL, routes as routesConfig } from "@/resources";
